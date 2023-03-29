f2cd44adbff254347a139e834c5a06443338e0f7
Valerija Ivanova <x22209042@student.ncirl.ie>
2023-03-30 00:04:44 +0100

    Week 9 Improvements

--- script.js
+++ script.js
@@ -1,63 +1,104 @@
+var attempt=0;
+var win=false;
+var comOutcome="";
+
 function random() {
 	
 	event.preventDefault();
 	
-	var userGesture=document.getElementById("gesture").value;
+	attempt=attempt+1;
 	
-	var comGestureRoll=Math.random();
-	
-	if (comGestureRoll<=0.33) {
-		comGestureRoll="Rock";
+	if(attempt<4 && win==false){
 		
-		if (userGesture=="Rock" || userGesture=="rock"){
-			var outcome="It's a draw!";
+		var lowerCase=document.getElementById("gesture").value;
+		var userGesture=lowerCase.toLowerCase();
+		
+		document.getElementById("comChoose").innerHTML="Here's what the computer chose!";
+		
+		var comGestureRoll=Math.random();
+		
+		while(comGestureRoll<=0.33 && comOutcome=="Rock") {
+			var comGestureRoll=Math.random();
 		}
-		else if (userGesture=="Paper" || userGesture=="paper"){
-			var outcome="You win!";
+		
+		while(comGestureRoll>=0.34 && comGestureRoll<=0.66 && comOutcome=="Paper") {
+			var comGestureRoll=Math.random();
 		}
-		else if (userGesture=="Scissors" || userGesture=="scissors"){
-			var outcome="You lose!";
+		
+		while(comGestureRoll>=67 && comOutcome=="Scissors") {
+			var comGestureRoll=Math.random();
+		}
+		
+		
+		if (comGestureRoll<=0.33) {
+			comOutcome="Rock";
+			
+			if (userGesture=="rock"){
+				var outcome="It's a draw!";
+			}
+			else if (userGesture=="paper"){
+				var outcome="You win!";
+				win=true;
+			}
+			else if (userGesture=="scissors"){
+				var outcome="You lose!";
+			}
+			else {
+				var outcome="Oops, wrong input! Try again.";
+				attempt=attempt-1;
+			}
+			
+		}
+		else if(comGestureRoll>=0.34 && comGestureRoll<=0.66) {
+			comOutcome="Paper";
+			
+			if (userGesture=="rock"){
+				var outcome="You lose!";
+			}
+			else if (userGesture=="paper"){
+				var outcome="It's a draw!";
+			}
+			else if (userGesture=="scissors"){
+				var outcome="You win!";
+				win=true;
+			}
+			else {
+				var outcome="Oops, wrong input! Try again.";
+				attempt=attempt-1;
+			}
 		}
 		else {
-			var outcome="Oops, wrong input! Try again.";
-		}
+			comOutcome="Scissors";
+			
+			if (userGesture=="rock"){
+				var outcome="You win!";
+				win=true;
+			}
+			else if (userGesture=="paper"){
+				var outcome="You lose!";
+			}
+			else if (userGesture=="scissors"){
+				var outcome="It's a draw!";
+			}
+			else {
+				var outcome="Oops, wrong input! Try again.";
+				attempt=attempt-1;
+			}
+		} 
+		
+		document.getElementById("computer").innerHTML=comOutcome;
+		
+		document.getElementById("outcome").innerHTML=outcome;
 		
 	}
-	else if(comGestureRoll>=0.34 && comGestureRoll<=0.66) {
-		comGestureRoll="Paper";
+	else{
 		
-		if (userGesture=="Rock" || userGesture=="rock"){
-			var outcome="You lose!";
-		}
-		else if (userGesture=="Paper" || userGesture=="paper"){
-			var outcome="It's a draw!";
-		}
-		else if (userGesture=="Scissors" || userGesture=="scissors"){
-			var outcome="You win!";
-		}
-		else {
-			var outcome="Oops, wrong input! Try again.";
-		}
+		document.getElementById("comChoose").innerHTML="Sorry, refresh the page to play again.";
+		
+		document.getElementById("computer").innerHTML="";
+		
+		document.getElementById("outcome").innerHTML="";
+		
 	}
-	else {
-		comGestureRoll="Scissors";
-		
-		if (userGesture=="Rock" || userGesture=="rock"){
-			var outcome="You win!";
-		}
-		else if (userGesture=="Paper" || userGesture=="paper"){
-			var outcome="You lose!";
-		}
-		else if (userGesture=="Scissors" || userGesture=="scissors"){
-			var outcome="It's a draw!";
-		}
-		else {
-			var outcome="Oops, wrong input! Try again.";
-		}
-	} 
-	
-	document.getElementById("computer").innerHTML=comGestureRoll;
-	
-	document.getElementById("outcome").innerHTML=outcome;
 	
 }
